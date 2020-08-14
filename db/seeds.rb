7f2d8031b6826687560d820f395dec4cf8423de7
pudu87 <sam.cappelle87@gmail.com>
2020-08-14 15:28:50 -0400

    add users controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users_list = [
+  %w(Rocky Peeters rocky@internet.com),
+  %w(Ivan Perisic ivan@internet.com),
+  %w(Wagneau Eloi wagneau@internet.com),
+  %w(Damir Mirvic damir@internet.com),
+  %w(Mama Dissa mama@internet.com),
+  %w(Jurgen Sierens jurgen@internet.com)
+]
+
+users_list.each do |f,l,e|
+  User.create(first_name: f, last_name: l, email: e, password: 'password', password_confirmation: 'password')
+end
