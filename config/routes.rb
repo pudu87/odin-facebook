d22baf1bc0e216a46c918cae0b3659464d9c1203
pudu87 <sam.cappelle87@gmail.com>
2020-08-17 15:35:28 -0400

    add login, logout, signup and edit links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup'}
 
   root to: 'users#index'
 
